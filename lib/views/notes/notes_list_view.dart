85270804a05564db7510582486538078cdd8b9e6
JoakimEineving <Joakim.eineving@gmail.com>
2022-07-29 02:57:21 +0200

    step-21

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mynotes/services/crud/notes_service.dart';
+import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
 
@@ -22,7 +22,7 @@ class NotesListView extends StatelessWidget {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        final note = notes[index];
+        final note = notes.elementAt(index);
         return Card(
           elevation: 6,
           margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
