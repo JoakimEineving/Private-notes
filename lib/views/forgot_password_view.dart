8fc47d5de93cb5746c726a9528b47045642a2f50
joei1029 <73241129+joei1029@users.noreply.github.com>
2022-07-29 20:02:50 +0200

    step-23

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -1,15 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
-class ForgotPasswordView extends StatelessWidget {
-  static String id = 'forgot-password';
-
+class ForgotPasswordView extends StatefulWidget {
   const ForgotPasswordView({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordView> createState() => _ForgotPasswordViewState();
+}
+
+class _ForgotPasswordViewState extends State<ForgotPasswordView> {
+  late final TextEditingController _email;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: Colors.white,
       body: Form(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -18,34 +40,53 @@ class ForgotPasswordView extends StatelessWidget {
             children: [
               const Text(
                 'Password reset',
-                style: TextStyle(fontSize: 30, color: Colors.white),
+                style: TextStyle(fontSize: 30, color: Colors.blueGrey),
               ),
               TextFormField(
-                style: const TextStyle(color: Colors.white),
+                controller: _email,
+                enableSuggestions: false,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                style: const TextStyle(color: Colors.blueGrey),
                 decoration: const InputDecoration(
                   labelText: 'Email',
                   icon: Icon(
                     Icons.mail,
-                    color: Colors.white,
+                    color: Colors.blueGrey,
                   ),
-                  errorStyle: TextStyle(color: Colors.white),
-                  labelStyle: TextStyle(color: Colors.white),
-                  hintStyle: TextStyle(color: Colors.white),
+                  errorStyle: TextStyle(color: Colors.blueGrey),
+                  labelStyle: TextStyle(color: Colors.blueGrey),
+                  hintStyle: TextStyle(color: Colors.blueGrey),
                   focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                    borderSide: BorderSide(color: Colors.blueGrey),
                   ),
                   enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                    borderSide: BorderSide(color: Colors.blueGrey),
                   ),
                   errorBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                    borderSide: BorderSide(color: Colors.blueGrey),
                   ),
                 ),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                child: const Text('Send Email'),
-                onPressed: () {},
+                child: const Text(
+                  'Send Email',
+                ),
+                onPressed: () async {
+                  final email = _email.text;
+                  try {
+                    await FirebaseAuth.instance.sendPasswordResetEmail(
+                      email: email,
+                    );
+                    Navigator.of(context).pushNamed(loginRoute);
+                  } on FirebaseAuthException {
+                    await showErrorDialog(
+                      context,
+                      'User not found',
+                    );
+                  }
+                },
               ),
               TextButton(
                 onPressed: () {
@@ -54,7 +95,7 @@ class ForgotPasswordView extends StatelessWidget {
                     (route) => false,
                   );
                 },
-                child: const Text('Login here!'),
+                child: const Text('Go back'),
               )
             ],
           ),
@@ -63,3 +104,88 @@ class ForgotPasswordView extends StatelessWidget {
     );
   }
 }
+
+
+
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:mynotes/constants/routes.dart';
+// import 'package:mynotes/services/auth/auth_exceptions.dart';
+// import 'package:mynotes/services/auth/auth_service.dart';
+// import 'package:mynotes/utilities/dialogs/error_dialog.dart';
+
+// class ForgotPasswordView extends StatefulWidget {
+//   static String id = 'forgot-password';
+  
+  
+//   const ForgotPasswordView({Key? key}) : super(key: key);
+
+//   @override
+//   State<ForgotPasswordView> createState() => _ForgotPasswordViewState();
+// }
+
+// class _ForgotPasswordViewState extends State<ForgotPasswordView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.lightBlueAccent,
+//       body: Form(
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               const Text(
+//                 'Password reset',
+//                 style: TextStyle(fontSize: 30, color: Colors.blueGrey),
+//               ),
+//               TextFormField(
+                
+//                 style: const TextStyle(color: Colors.blueGrey),
+//                 decoration: const InputDecoration(
+//                   labelText: 'Email',
+//                   icon: Icon(
+//                     Icons.mail,
+//                     color: Colors.blueGrey,
+//                   ),
+//                   errorStyle: TextStyle(color: Colors.blueGrey),
+//                   labelStyle: TextStyle(color: Colors.blueGrey),
+//                   hintStyle: TextStyle(color: Colors.blueGrey),
+//                   focusedBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.blueGrey),
+//                   ),
+//                   enabledBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.blueGrey),
+//                   ),
+//                   errorBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.blueGrey),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 20),
+//               ElevatedButton(
+                
+//                 child: const Text('Send Email',),
+                
+//                 onPressed: () {
+                  
+//                   FirebaseAuth.instance.sendPasswordResetEmail(email: _email);
+//                 },
+//               ),
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pushNamedAndRemoveUntil(
+//                     loginRoute,
+//                     (route) => false,
+//                   );
+//                 },
+//                 child: const Text('Login here!'),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
