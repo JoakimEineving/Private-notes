8e0a4ec6b80b9a2895b6217b835767982c854746
joei1029 <73241129+joei1029@users.noreply.github.com>
2022-07-30 19:58:58 +0200

    step-24

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -79,7 +79,26 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
                     await FirebaseAuth.instance.sendPasswordResetEmail(
                       email: email,
                     );
-                    Navigator.of(context).pushNamed(loginRoute);
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Email sent"),
+                          content: const Text(
+                              "Please check your email to reset your password"),
+                          actions: <Widget>[
+                            ElevatedButton(
+                              child: const Text("OK"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
                   } on FirebaseAuthException {
                     await showErrorDialog(
                       context,
@@ -104,88 +123,3 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
     );
   }
 }
-
-
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:mynotes/constants/routes.dart';
-// import 'package:mynotes/services/auth/auth_exceptions.dart';
-// import 'package:mynotes/services/auth/auth_service.dart';
-// import 'package:mynotes/utilities/dialogs/error_dialog.dart';
-
-// class ForgotPasswordView extends StatefulWidget {
-//   static String id = 'forgot-password';
-  
-  
-//   const ForgotPasswordView({Key? key}) : super(key: key);
-
-//   @override
-//   State<ForgotPasswordView> createState() => _ForgotPasswordViewState();
-// }
-
-// class _ForgotPasswordViewState extends State<ForgotPasswordView> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.lightBlueAccent,
-//       body: Form(
-//         child: Padding(
-//           padding: const EdgeInsets.symmetric(horizontal: 30.0),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               const Text(
-//                 'Password reset',
-//                 style: TextStyle(fontSize: 30, color: Colors.blueGrey),
-//               ),
-//               TextFormField(
-                
-//                 style: const TextStyle(color: Colors.blueGrey),
-//                 decoration: const InputDecoration(
-//                   labelText: 'Email',
-//                   icon: Icon(
-//                     Icons.mail,
-//                     color: Colors.blueGrey,
-//                   ),
-//                   errorStyle: TextStyle(color: Colors.blueGrey),
-//                   labelStyle: TextStyle(color: Colors.blueGrey),
-//                   hintStyle: TextStyle(color: Colors.blueGrey),
-//                   focusedBorder: UnderlineInputBorder(
-//                     borderSide: BorderSide(color: Colors.blueGrey),
-//                   ),
-//                   enabledBorder: UnderlineInputBorder(
-//                     borderSide: BorderSide(color: Colors.blueGrey),
-//                   ),
-//                   errorBorder: UnderlineInputBorder(
-//                     borderSide: BorderSide(color: Colors.blueGrey),
-//                   ),
-//                 ),
-//               ),
-//               const SizedBox(height: 20),
-//               ElevatedButton(
-                
-//                 child: const Text('Send Email',),
-                
-//                 onPressed: () {
-                  
-//                   FirebaseAuth.instance.sendPasswordResetEmail(email: _email);
-//                 },
-//               ),
-//               TextButton(
-//                 onPressed: () {
-//                   Navigator.of(context).pushNamedAndRemoveUntil(
-//                     loginRoute,
-//                     (route) => false,
-//                   );
-//                 },
-//                 child: const Text('Login here!'),
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
