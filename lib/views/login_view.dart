f1484be0b34cac0c8b5dbcd2faf219fc95205fd7
JoakimEineving <Joakim.eineving@gmail.com>
2022-08-02 15:48:02 +0200

    add bloc authentication

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -119,24 +122,12 @@ class _HomePageState extends State<LoginView> {
                       final email = _email.text;
                       final password = _password.text;
                       try {
-                        await AuthService.firebase().logIn(
-                          email: email,
-                          password: password,
-                        );
-                        final user = AuthService.firebase().currentUser;
-                        if (user?.isEmailVerified ?? false) {
-                          //user's email IS verified
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                            notesRoute,
-                            (route) => false,
-                          );
-                        } else {
-                          //user's email NOT verified
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                            verifyEmailRoute,
-                            (route) => false,
-                          );
-                        }
+                        context.read<AuthBloc>().add(
+                              AuthEventLogIn(
+                                email,
+                                password,
+                              ),
+                            );
                       } on UserNotFoundAuthException {
                         await showErrorDialog(
                           context,
